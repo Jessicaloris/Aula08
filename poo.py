cfcdb5be9518016fe7f07907e3328fb98161deea
Jessica Loris <dearjess535@gmail.com>
2025-05-06 08:48:00 -0300

    anotações

---
+++ poo.py
@@ -0,0 +1,10 @@
+#classe = forma/projeto - estrutura para criar objetos.
+"""
+classe:
+1- atributos (todo atributo é uma variavel)
+2- metodos (tudo aquilo que é uma ação, metodo são funções)
+função = quando possui um return
+procedimento = quando não possui return
+
+
+"""
